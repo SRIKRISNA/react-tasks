ca23e9c07b8b1c408f53772c4581e96cf56c5aad
Srikrishna <krishna333sp@gmail.com>
2022-11-21 23:58:07 +0530

    webpage static completed

--- src/components/webpage/main.jsx
+++ src/components/webpage/main.jsx
@@ -1,14 +1,31 @@
 import WebHeader from "./header";
 import WebSidebar from "./sidebar";
+import img2 from "./home1.jpg";
 
-function Webpage(){
-    return(
-        <>
-            <WebHeader/>
-            <WebSidebar/>
-            <h1>Main page</h1>
-            
-        </>
+function Webpage() {
+    return (
+        <div style={{background:"#fff", padding:"0px 20px", margin:"20px 50px"}}>
+            <WebHeader />
+            <div className="mainn">
+                <div className="main-container">
+                    <div className="upper-sec">
+                        <div>
+                            <h1 style={{fontSize:"65px"}}>KAGE NORE.</h1>
+                        </div>
+                        <div id="right-name">
+                            <p>Abbdch crjn</p>
+                            <p>Sunny G</p>
+                        </div>
+                    </div>
+                    <div id="img-sec">
+                        <img src={img2} height="500px"></img>
+                    </div>
+                </div>
+                <div className="sideb">
+                    <WebSidebar />
+                </div>
+            </div>
+        </div>
     )
 }
 export default Webpage;
--- src/components/webpage/sidebar.jsx
+++ src/components/webpage/sidebar.jsx
@@ -1,9 +1,18 @@
 
-
-function WebSidebar(){
-    return(
+import img1 from "./home2.jpg";
+function WebSidebar() {
+    return (
         <>
-            <h1>Sidebar</h1>
+            <div className="sidebar-container">
+                <div style={{textAlign:"center", margin:"0px 0px 150px 0px"}}>
+                    <p>Antony M</p>
+                    <p>Sunny G</p>
+                </div>
+                <div id="img-sec">
+                    <img src={img1} height="200px"></img>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo dolorum adipisci minima nostrum explicabo culpa aperiam dolore nulla quasi dicta?</p>
+                </div>
+            </div>
         </>
     )
 }
